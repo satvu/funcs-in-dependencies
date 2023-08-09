008e30cffb20ff6a06cf692b2c6a4935da6159f4
Sarah Vu <sarahvu@microsoft.com>
2023-08-09 16:00:50 -0700

    namespae change

--- LibraryWithFunctions/Class1.cs
+++ LibraryWithFunctions/Class1.cs
@@ -3,7 +3,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
-namespace AppWithFunctionsInDependencies
+namespace LibraryWithFunctions
 {
     public class DependencyFunc
     {
